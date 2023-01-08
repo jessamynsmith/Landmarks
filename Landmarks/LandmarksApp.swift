991c6d9962f5e58dfe3d7945868197a5037e0673
jessamynsmith <geekchick77@gmail.com>
2023-01-08 11:57:31 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jessamyn Smith on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
